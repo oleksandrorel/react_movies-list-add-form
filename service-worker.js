a8c516fa8686e4c6ba0479bcfba5adf8f9045f56
Oleksandr Orel <oleksandrorel@ukr.net>
2021-03-05 11:00:33 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.640a1ae90289d943d61366c4c13857aa.js"
 );
 
 self.addEventListener('message', (event) => {
